f41782fe82d3772e532b3ecf446f6ba5dbc8c6f5
Sutas Nakasawek <sutas@arterex.com>
2016-06-14 16:32:32 +0700

    Release 0.2.0

--- src/index.js
+++ src/index.js
@@ -4,13 +4,22 @@ import * as Promise from 'bluebird';
 const storageGet = Promise.promisify(storage.get);
 const storageSet = Promise.promisify(storage.set);
 
-export default (key) => ({
+export default (key, backup) => ({
     load() {
         return storageGet(key).then((jsonState) => JSON.parse(jsonState) || {});
     },
 
     save(state) {
         const jsonState = JSON.stringify(state);
+
+        if (backup !== undefined) {
+            return new Promise((resolve) => {
+                storageSet('backup.' + key, jsonState).then(() => {
+                    storageSet(key, jsonState).then(resolve);
+                });
+            });
+        }
+
         return storageSet(key, jsonState);
     }
 });
